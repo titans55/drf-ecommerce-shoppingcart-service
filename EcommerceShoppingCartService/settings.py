ba0407c7be420a0659f7a9169909a7bada2eec20
kutay <kutayaslanfb@hotmail.com>
2020-07-19 21:10:14 +0300

    dockerized

--- EcommerceShoppingCartService/settings.py
+++ EcommerceShoppingCartService/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
